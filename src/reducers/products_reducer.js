fc99f20cad21666d4fd2ee88fcaac2b2e9a3deca
Sachin Verma <sachinverma53121@gmail.com>
2021-02-25 22:09:33 +0530

    fetch

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -1,25 +1,21 @@
-
-
-const products_reducer = (state,action) =>{
-
-    if(action.type==='SET_AREA'){
-        return {...state,area:action.payload}
+const products_reducer = (state, action) => {
+  if (action.type === "SET_AREA") {
+    return { ...state, area: action.payload };
+  }
+  if (action.type === "SET_MANDIS") {
+    const { area, all_mandis } = state;
+    //console.log(area);
+    let tempmandis;
+    if (area === "all") {
+      tempmandis = all_mandis;
+    } else {
+      tempmandis = all_mandis.filter((mandi) => mandi.area === area);
     }
-    if(action.type==='SET_MANDIS'){
-        const {area,all_mandis}=state;
-        //console.log(area);
-        let tempmandis
-        if(area==='all')
-        {
-            tempmandis=all_mandis
-        }else{
-            tempmandis=all_mandis.filter((mandi)=>mandi.area===area)
-        }
-        return {...state,mandis:tempmandis}
-    }
-    return state
-    throw new Error(`No Matching "${action.type}" - action type `)
+    return { ...state, mandis: tempmandis };
+  }
 
-}
+  throw new Error(`No Matching "${action.type}" - action type `);
+  return state;
+};
 
-export default products_reducer
+export default products_reducer;
