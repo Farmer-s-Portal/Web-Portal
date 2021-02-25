9f7f0222bb61a88999a6db3b712b48889cbf987a
samarth <45564142+samarthnsut@users.noreply.github.com>
2021-02-25 21:57:38 +0530

    mandi page setup

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -13,46 +13,9 @@ const HomePage = () =>{
 
   return (
     <main>
-      <h1>home page</h1>
+      <h1>Landing page bna lo bsdvalo</h1>
       <Hero/>
-      <FormGroup style={{width:"320px", margin:"auto"}}>
-        <Form.Control as="select" name="area" value={area} onChange={setarea}>
-        <option value='all'>Select the mandi which best suits you</option>
-        <option value='South West Delhi'>South West Delhi</option>
-        <option value='Rewari,Haryana'>Rewari,Haryana</option>  
-        </Form.Control>
-      </FormGroup>
-      <section className='container'>
-      <div className='row'>
-        {mandis.map((mandi,index)=>{
-          const {name,location,shops,rating,area,reviews}=mandi;
-            return (
-              <div className='col-lg-3 col-md-4 col-sm-6 col-xsm-12 m-4'>
-              <Card style={{ width: '18rem' }} className="shadow-sm">
-                <div className="rating position-absolute p-2" style={{right:"10px", top:"10px", borderRadius:"10px", backgroundColor:"lightgrey", fontWeight:"600"}}>
-                <FontAwesomeIcon icon={faStar} className="mr-1" style={{color:"goldenrod"}}></FontAwesomeIcon>
-                  {rating}
-                </div>
-                <Card.Img variant="top" src="https://aniportalimages.s3.amazonaws.com/media/details/EVsOuIGU0AAWoqq_6kP82gF.jpg" />
-                <Card.Body>
-                  <Card.Title>{name.toUpperCase()}</Card.Title>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                  <ListGroupItem>
-                      <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-4"></FontAwesomeIcon>
-                      {location}
-                  </ListGroupItem>
-                  <ListGroupItem><FontAwesomeIcon icon={faStore} className="mr-3"></FontAwesomeIcon>There are {shops} shops</ListGroupItem>
-                </ListGroup>
-                <Card.Body>
-                <Button variant="outline-success" block>Enter {name.toUpperCase()}</Button>
-                </Card.Body>
-              </Card>
-              </div>
-            )
-        })}
-        </div>
-      </section>
+     
     </main>
   )
 }
---
+++ src/pages/MandiPage.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {Card,Button, ListGroup, ListGroupItem, FormGroup, Form} from "react-bootstrap"
+import {useProductsContext} from '../contexts/product_context'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMapMarkerAlt,
+  faStore,
+  faStar
+} from "@fortawesome/free-solid-svg-icons";
+
+
+function MandiPage() {
+    const {area,setarea,mandis} = useProductsContext();
+  return (
+    <div>
+       <FormGroup style={{width:"320px", margin:"auto"}}>
+        <Form.Control as="select" name="area" value={area} onChange={setarea}>
+        <option value='all'>Select the mandi which best suits you</option>
+        <option value='South West Delhi'>South West Delhi</option>
+        <option value='Rewari,Haryana'>Rewari,Haryana</option>  
+        </Form.Control>
+      </FormGroup>
+      <section className='container'>
+      <div className='row'>
+        {mandis.map((mandi,index)=>{
+          const {name,location,shops,rating,area,reviews}=mandi;
+            return (
+              <div className='col-lg-3 col-md-4 col-sm-6 col-xsm-12 m-4'>
+              <Card style={{ width: '18rem' }} className="shadow-sm">
+                <div className="rating position-absolute p-2" style={{right:"10px", top:"10px", borderRadius:"10px", backgroundColor:"lightgrey", fontWeight:"600"}}>
+                <FontAwesomeIcon icon={faStar} className="mr-1" style={{color:"goldenrod"}}></FontAwesomeIcon>
+                  {rating}
+                </div>
+                <Card.Img variant="top" src="https://aniportalimages.s3.amazonaws.com/media/details/EVsOuIGU0AAWoqq_6kP82gF.jpg" />
+                <Card.Body>
+                  <Card.Title>{name.toUpperCase()}</Card.Title>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem>
+                      <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-4"></FontAwesomeIcon>
+                      {location}
+                  </ListGroupItem>
+                  <ListGroupItem><FontAwesomeIcon icon={faStore} className="mr-3"></FontAwesomeIcon>There are {shops} shops</ListGroupItem>
+                </ListGroup>
+                <Card.Body>
+                <Button variant="outline-success" block>Enter {name.toUpperCase()}</Button>
+                </Card.Body>
+              </Card>
+              </div>
+            )
+        })}
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default MandiPage;
