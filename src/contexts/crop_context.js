c30d6c9a98be66ecd8accaa891b7cbc7dfd96372
Sachin Verma <sachinverma53121@gmail.com>
2021-03-05 11:42:05 +0530

    bug fix

--- src/contexts/crop_context.js
+++ src/contexts/crop_context.js
@@ -1,55 +1,86 @@
 import React, { useContext, useEffect, useReducer, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import firebase, { fire } from "../firebase";
-import reducer from '../reducers/crops_reducer';
+import reducer from "../reducers/crops_reducer";
 import { useUserContext } from "./user_context";
 
-
 const initalState = {
-    allCrops: [{comm:"kela", price:"500",quantity:"100",city:"Hyderabad",state:"AP",zip:"530065",user:{name:"Dagar"}}]
-    // allCrops: []
-}
+  allCrops: [
+    {
+      comm: "kela",
+      price: "500",
+      quantity: "100",
+      city: "Hyderabad",
+      state: "AP",
+      zip: "530065",
+      user: { name: "Dagar" },
+    },
+  ],
+  loading: true,
+  // allCrops: []
+};
 const CropContext = React.createContext();
 
-export const CropProvider = ({children}) => {
-    const {currentUser} = useUserContext();
-    const [state, dispatch] = useReducer(reducer, initalState);
-    const getAllCrops = ()=>{
-        let temp = [];
-        fire.collection('crops').get().then(function(data){
-            data.docs.forEach(crop => {
-                crop.data().user.get().then(function(res){
-                    console.log("res",res.data())
-                    let temp2=crop.data();
-                    temp2={...temp2,user:res.data()}
-                    temp.push(temp2);
-                })
-            })
-        });
-        return temp;
-    }
-    useEffect(async() => {
-        let data = await getAllCrops();
-        dispatch({type:"SET_CROPS", payload: data});
-    },[]);
-    const createCrop = async (values) => {
-        console.log(values);
-        values={...values,
-            user:fire.collection('users').doc(currentUser.uid)}
-        try {
-            const res = await fire.collection('crops').add(values);
-            const ref= fire.collection('crops').doc(res.id)
-            const res2=await fire.collection('users').doc(currentUser.uid)
-                     .update({
-                         posts: firebase.firestore.FieldValue.arrayUnion(ref)                    })
-        } catch (error) {
-            alert("Error in writing DB");
-            console.log(error);
-        }
-    }
-    return <CropContext.Provider value={{...state, createCrop, getAllCrops}}>{children}</CropContext.Provider>
-}
+export const CropProvider = ({ children }) => {
+  const { currentUser } = useUserContext();
+  const [state, dispatch] = useReducer(reducer, initalState);
 
-export const useCropContext = () =>{
-    return useContext(CropContext);
-}
+  const getAllCrops = () => {
+    dispatch({ type: "SET_LOADING" });
+    let temp = [];
+    fire
+      .collection("crops")
+      .get()
+      .then(async (data) => {
+        for (let i = 0; i < data.docs.length; ++i) {
+          const crop = data.docs[i];
+          await crop
+            .data()
+            .user.get()
+            .then(async (res) => {
+              console.log("res", res.data());
+              let temp2 = await crop.data();
+              temp2 = { ...temp2, user: res.data() };
+              temp.push(temp2);
+
+              console.log("end get crp 2nd then");
+            });
+        }
+
+        dispatch({ type: "SET_CROPS", payload: temp });
+        dispatch({ type: "NOT_LOADING" });
+        console.log("end get crp 1st then");
+      });
+  };
+
+  useEffect(() => {
+    getAllCrops();
+  }, []);
+
+  const createCrop = async (values) => {
+    console.log(values);
+    values = { ...values, user: fire.collection("users").doc(currentUser.uid) };
+    try {
+      const res = await fire.collection("crops").add(values);
+      const ref = fire.collection("crops").doc(res.id);
+      const res2 = await fire
+        .collection("users")
+        .doc(currentUser.uid)
+        .update({
+          posts: firebase.firestore.FieldValue.arrayUnion(ref),
+        });
+    } catch (error) {
+      alert("Error in writing DB");
+      console.log(error);
+    }
+  };
+  return (
+    <CropContext.Provider value={{ ...state, createCrop, getAllCrops }}>
+      {children}
+    </CropContext.Provider>
+  );
+};
+
+export const useCropContext = () => {
+  return useContext(CropContext);
+};
--- src/reducers/crops_reducer.js
+++ src/reducers/crops_reducer.js
@@ -1,10 +1,17 @@
 const crops_reducer = (state, action) => {
-    if (action.type === "SET_CROPS") {
-      return { ...state, allCrops: action.payload }
-    }
-    throw new Error(`No Matching "${action.type}" - action type `);
-    return state;
-  };
-  
-  export default crops_reducer;
-  
+  if (action.type === "SET_CROPS") {
+    return { ...state, allCrops: action.payload };
+  }
+
+  if (action.type === "SET_LOADING") {
+    return { ...state, loading: true };
+  }
+
+  if (action.type === "NOT_LOADING") {
+    return { ...state, loading: false };
+  }
+  throw new Error(`No Matching "${action.type}" - action type `);
+  return state;
+};
+
+export default crops_reducer;
