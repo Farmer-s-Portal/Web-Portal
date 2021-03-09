2af40e4807fa1ec8743a7b842244c5f013b831ae
Dikshant Rajwal <drajwal1511@gmail.com>
2021-03-09 19:35:56 +0530

    profilepage modified

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,5 @@
 import React, {useRef,useState} from "react";
-import { Form } from "react-bootstrap";
+import { Col, Form, Row } from "react-bootstrap";
 import { useUserContext } from "../contexts/user_context";
 import {Button} from 'react-bootstrap'
 function ProfilePage(){
@@ -9,53 +9,64 @@ function ProfilePage(){
             <div className='pro-back' style={{textAlign:'left',width:'80%',margin:'20px auto',padding:'20px'}}>
             <h1 style={{color:'#5c342f'}}>Account Profile</h1>
             <hr></hr>
-            <Form.Group>
-            <Form.Label style={{color:'#5c342f'}}>
+            <Form>
+            <Form.Group as={Row}>
+            <Form.Label column sm="2"  style={{color:'#5c342f'}}>
                 Name
             </Form.Label>
+            <Col sm="10">
             <Form.Control
             style={{color:'black'}}
             type="text"
             defaultValue={currentUser.name}
             disabled
             />
+            </Col>
             </Form.Group>
 
-            <Form.Group>
-            <Form.Label style={{color:'#5c342f'}}>
-                Phone Number
+            <Form.Group as={Row}>
+            <Form.Label column sm="2" style={{color:'#5c342f'}}>
+                P.No.
             </Form.Label>
+            <Col sm="10">
             <Form.Control
             type="text"
             style={{color:'black'}}
             defaultValue={currentUser.phone}
             disabled
             />
+            </Col>
             </Form.Group>
 
-            <Form.Group>
-            <Form.Label style={{color:'#5c342f'}}>
+            <Form.Group as={Row}>
+            <Form.Label column sm="2" style={{color:'#5c342f'}}>
                 Address
             </Form.Label>
+            <Col sm="10">
             <Form.Control
-            type="text"
+            as="textarea"
+            rows={3}
             style={{color:'black'}}
             defaultValue={currentUser.address}
             disabled
             />
+            </Col>
             </Form.Group>
 
-            <Form.Group>
-            <Form.Label style={{color:'#5c342f'}}>
+            <Form.Group as={Row}>
+            <Form.Label column sm="2" style={{color:'#5c342f'}}>
                 User Type
             </Form.Label>
+            <Col sm="10">
             <Form.Control
             type="text"
             style={{color:'black'}}
             defaultValue={currentUser.type}
             disabled
             />
+            </Col>
             </Form.Group>
+            </Form>
             <br></br>
             <Button  variant='outline-success'>Update</Button>
             <hr></hr>
